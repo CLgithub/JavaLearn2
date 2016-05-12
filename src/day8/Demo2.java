cc8c0ef56a65abb11a563b733839002ee184e0ae
CLgithub <1064134436@qq.com>
2016-05-12 21:36:20 +0800

    Schema约束

---
+++ src/day8/Demo2.java
@@ -0,0 +1,18 @@
+package day8;
+
+/*
+	Schema约束
+	
+	名称空间：告诉xml文档的元素被哪个sahema文档约束
+	
+	相比dtd的优点，在一个xml文档中，可以收到不同xsd的约束
+	
+	1）一个名称空间受到一个schema文档约束
+	2）一个名称空间受到多个schema文档约束
+	3）默认名称空间
+	4）没有名称空间
+	
+*/
+public class Demo2 {
+
+}
