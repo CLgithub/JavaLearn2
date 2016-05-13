a4f621633a2c217d34bf34221e9d0f4cc80e1218
CLgithub <1064134436@qq.com>
2016-05-13 21:12:43 +0800

    tomcat和web工厂目录结构

--- src/day8_web/Demo2.java
+++ src/day8_web/Demo2.java
@@ -56,6 +56,34 @@ package day8_web;
 						1521： orace数据库的端口。
 			/myweb:   web应用的名称。默认情况下，在webapps目录下找
 			/test.html  ： 资源名称。
+
+5.Tomcat的目录结构
+	|-bin: 存放tomcat的命令。
+			catalina.bat 命令：
+				startup.bat  -> catalina.bat start	
+				shutdown.bat - > catalina.bat stop
+    |- conf: 存放tomcat的配置信息。其中server.xml文件是核心的配置文件。
+	|-lib：支持tomcat软件运行的jar包。其中还有技术支持包，如servlet，jsp
+	|-logs：运行过程的日志信息
+	|-temp: 临时目录
+	|-webapps： 共享资源目录。web应用目录。（注意不能以单独的文件进行共享）
+	|-work： tomcat的运行目录。jsp运行时产生的临时文件就存放在这里
+	
+
+6.WEB应用的目录结构
+	|-WebRoot:web应用的根目录
+		|-静态资源（html，css，js，image……）
+		|-WEB-INF：固定写法
+			|-classes:(可选)固定写法。存放class字节码文件
+			|-lib：（可选）固定写法。存放jar包文件。
+			|-web.xml
+			|- TLD文件：标签库描述文件
+
+	注意：
+		1.WEB-INF目录里的资源不能通过浏览器直接访问
+		2.如果希望访问到WEB-INF里面的资源，就必须把资源配置到一个叫web.xml的文件中。
+
+
 */
 public class Demo2 {
 
