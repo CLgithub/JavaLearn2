7a7c9f21e8f536e73aaf36bab4d533700345b319
CLgithubSZ <1064134436@qq.com>
2016-06-30 15:23:35 +0800

    mybatis：mybatis的引入及jdbc开发存在的问题

---
+++ src/day72_mybatis/Doc1.java
@@ -0,0 +1,36 @@
+package day72_mybatis;
+
+/*
+mybatis是一个java持久层框架，java中操作关系型 数据库用的是jdbc，mybatis是对jdbc的一个封装。
+
+1、从一个jdbc程序开始，通过jdbc 程序找到使用原生态的jdbc开发程序，存在哪些问题？？
+	通过学习mybatis，mybatis是如何解决这些问题。
+2、mybatis的架构（重点）
+3、mybatis入门程序（重点）
+	实现用户的查询、添加、修改、删除
+4、mybatis开发dao的两种方法（重点）
+	原始的dao开发方式（dao接口和dao实现都需要编写）
+	mapper代理开发（只需要写dao接口）
+5、输入映射类型和蔬菜映射类型
+6、动态sql
+
+第二天：
+	高级映射查询（一对一，一对多，多对多）
+	查询缓存
+	延时加载
+	mybatis和spring整合（重点）
+	mybatis逆向工程
+	
+项目驱动开发
+	先导入sql_table.sql，再导入sql_data.sql(记录系统的初始化数据)
+	通常需要提供初始化数据的数据库脚本。
+
+
+1.jdbc编程中的问题
+	在企业开发中，根据项目大小，特点进行技术选择，jdbc操作数据库时效率高，jdbc也是技术选择的参考
+	
+
+*/
+public class Doc1 {
+
+}
---
+++ src/day72_mybatis/demo/jdbc/JdbcTest.java
@@ -0,0 +1,79 @@
+package day72_mybatis.demo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+jdbc问题总结：
+	1、数据库连接频繁的创建和关闭，缺点浪费数据库的资源，影响操作效率
+		设想：使用数据库连接池
+	2、sql语句是硬编码，如果需求变更需要修改sql，就需要修改java代码，需要重新编译，系统不易维护。
+		设想：将sql语句 统一配置在文件中，修改sql不需要修改java代码。
+	3、通过preparedStatement向占位符设置参数，存在硬编码（ 参数位置，参数）问题。系统不易维护。
+		设想：将sql中的占位符及对应的参数类型配置在配置文件中，能够自动输入 映射。
+	4、遍历查询结果集存在硬编码（列名）。
+		设想：自动进行sql查询结果向java对象的映射（输出映射）。
+
+	
+*/
+public class JdbcTest {
+	
+	public static void main(String[] args) {
+		Connection connection = null;
+		// PreparedStatement是预编译的Statement，通过Statement发起数据库的操作
+		// PreparedStatement防止sql注入，执行数据库效率高
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			// 加载数据库驱动
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// 通过驱动管理类获取数据库链接
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis1?characterEncoding=utf-8",
+					"root", "123456");
+			// 定义sql语句 ?表示占位符
+			String sql = "select * from user where username = ?";
+			// 获取预处理statement
+			preparedStatement = connection.prepareStatement(sql);
+			// 设置参数，第一个参数为sql语句中参数的序号（从1开始），第二个参数为设置的参数值
+			preparedStatement.setString(1, "王五");
+			// 向数据库发出sql执行查询，查询出结果集
+			resultSet = preparedStatement.executeQuery();
+			// 遍历查询结果集
+			while (resultSet.next()) {
+				System.out.println(resultSet.getString("id") + "  " + resultSet.getString("username"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 释放资源
+			try {
+				if(resultSet!=null){
+					resultSet.close();
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			} finally{
+				try {
+					if(preparedStatement!=null){
+						preparedStatement.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally{
+					try {
+						if(connection!=null){
+							connection.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
