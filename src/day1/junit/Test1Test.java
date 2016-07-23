9f167c20954a745c3eadbb9b366d3bff0ec50057
clG450 <1064134436@qq.com>
2016-07-23 11:59:18 +0800

    复习

---
+++ src/day1/junit/Test1Test.java
@@ -0,0 +1,19 @@
+package day1.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Test1Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
